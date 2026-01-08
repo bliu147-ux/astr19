ce8a7e1207097c804778013760c2e64188b1ec25
bliu147-ux <bliu147@ucsc.edu>
2026-01-07 20:42:34 -0800

    Add files via upload

---
+++ journal2.py
@@ -0,0 +1,14 @@
+x = 4.1
+y = 2.6 # declaring to be floating point
+print(x + y) #printing their sum
+print("data type of ", type(x+y)) #finding the data type
+
+x_1 = 3
+x_2 = 4999      #declaring to be integers
+print(x_1 - x_2) #printing their difference
+print("data type of ", type(x_1 - x_2)) #finding the data type
+
+x_3 = 19    #declaring an integer
+x_4 = 4.6 #declaring a floating point
+print(x_3 * x_4)
+print("data type of ", type(x_3 * x_4)) #finding the data type

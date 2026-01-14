d04a3cb3c000083906956ddd56930b72d89cd3be
bliu147-ux <bliu147@ucsc.edu>
2026-01-13 22:12:27 -0800

    Add files via upload

---
+++ journal3.py
@@ -0,0 +1,10 @@
+def f(x):       #define a funciton
+	return x**3 + 8
+def main():    #main function
+
+	print(f(9))          #print first
+	#if(f(9)>27):         #if f(9) is greater than 27, print yay
+		#print('Yay!')
+
+if __name__ == '__main__':
+	main()
